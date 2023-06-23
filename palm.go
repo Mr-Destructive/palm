c06d4449d99ec1637775a1e0f62dcd359642129e
mr_destructive <gormeet711@gmail.com>
2023-06-23 19:28:07 +0530

    fix: remove main package

--- palm.go
+++ palm.go
@@ -2,7 +2,6 @@ package palm
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strings"
 )
@@ -30,54 +29,4 @@ func LoadEnvFromFile(filepath string) error {
 	}
 
 	return nil
-}
-
-func main() {
-	modelName := "text-bison-001"
-	/*
-		message := MessagePrompt{
-			Context: "what is the meaning of life",
-			Examples: []Example{
-				Example{
-					Input: Message{
-						Author:  "Palm",
-						Content: "what is the meaning of life",
-					},
-					Output: Message{
-						Author:  "Palm",
-						Content: "what is the meaning of life",
-					},
-				},
-			},
-			Messages: []Message{
-				Message{
-					Author:  "Palm",
-					Content: "what is the meaning of life",
-				},
-			},
-		}
-	*/
-	message := MessagePrompt{
-		Messages: []Message{
-			Message{
-				Content: "what is the meaning of life",
-			},
-		},
-	}
-	m, err := GenerateMessage(message, map[string]string{"model": "chat-bison-001"})
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(m)
-	model, err := GetModel(modelName)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(model)
-	ListModels()
-	text, err := GenerateText(modelName, PromptConfig{Prompt: TextPrompt{"what is the meaning of life"}})
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(text)
 }
